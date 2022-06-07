2e06b75b4dfd2d0834ea0ca71b05b64bdc005590
Virola <virola@foxmail.com>
2022-06-07 22:43:01 +0800

    update travis

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 5678,
-        open: false,
+        open: true,
         proxy: {
             // 本地开发环境通过代理实现跨域，生产环境使用 nginx 转发
             // 正则表达式写法
